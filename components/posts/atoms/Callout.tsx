d241d3ef8094947a02909b96adfa35f562336ab9
이민기 <mingi3442@gmail.com>
2025-08-14 19:59:23 +0900

    ✨ Add blog post about Tailwind CSS and Emotion internals

--- components/posts/atoms/Callout.tsx
+++ components/posts/atoms/Callout.tsx
@@ -88,7 +88,7 @@ const InfoIcon = () => (
 
 export const TipCallout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-green-300/20 dark:bg-green-700/30 border-l-4 border-green-500 rounded-md px-5 pb-0.5">
+    <div className="bg-green-300/20 dark:bg-green-700/30 border-l-4 border-green-500 rounded-md px-5 pb-0.5 mb-5">
       <div className="flex items-center pt-5">
         <span className="mr-2">
           <TipIcon />
@@ -102,7 +102,7 @@ export const TipCallout = ({ children }: { children: ReactNode }) => {
 
 export const NoteCallout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-blue-300/20 dark:bg-blue-700/30 border-l-4 border-blue-500 rounded-md px-5 pb-0.5">
+    <div className="bg-blue-300/20 dark:bg-blue-700/30 border-l-4 border-blue-500 rounded-md px-5 pb-0.5 mb-5">
       <div className="flex items-center pt-5">
         <span className="mr-2">
           <NoteIcon />
@@ -116,7 +116,7 @@ export const NoteCallout = ({ children }: { children: ReactNode }) => {
 
 export const WarningCallout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-yellow-300/20 dark:bg-yellow-700/30 border-l-4 border-yellow-500 rounded-md px-5 pb-0.5">
+    <div className="bg-yellow-300/20 dark:bg-yellow-700/30 border-l-4 border-yellow-500 rounded-md px-5 pb-0.5 mb-5">
       <div className="flex items-center pt-5">
         <span className="mr-2">
           <WarningIcon />
@@ -130,7 +130,7 @@ export const WarningCallout = ({ children }: { children: ReactNode }) => {
 
 export const ErrorCallout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-red-300/20 dark:bg-red-700/30 border-l-4 border-red-500 rounded-md px-5 pb-0.5">
+    <div className="bg-red-300/20 dark:bg-red-700/30 border-l-4 border-red-500 rounded-md px-5 pb-0.5 mb-5">
       <div className="flex items-center pt-5">
         <span className="mr-2">
           <ErrorIcon />
@@ -144,7 +144,7 @@ export const ErrorCallout = ({ children }: { children: ReactNode }) => {
 
 export const InfoCallout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="bg-indigo-300/20 dark:bg-indigo-700/30 border-l-4 border-indigo-500 rounded-md px-5 pb-0.5">
+    <div className="bg-indigo-300/20 dark:bg-indigo-700/30 border-l-4 border-indigo-500 rounded-md px-5 pb-0.5 mb-5">
       <div className="flex items-center pt-5">
         <span className="mr-2">
           <InfoIcon />
