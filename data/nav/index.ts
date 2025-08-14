a52d489a8414b8a9da2d824aee8c073f52201339
이민기 <mingi3442@gmail.com>
2025-08-14 20:14:19 +0900

    ✨ Remove Tags navigation link in header menu

--- data/nav/index.ts
+++ data/nav/index.ts
@@ -1,7 +1,7 @@
 export const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/posts', title: 'Posts' },
-  { href: '/tags', title: 'Tags' },
+  // { href: '/tags', title: 'Tags' },
   // { href: '/projects', title: 'Projects' },
   // { href: '/about', title: 'About' },
   { href: 'https://resume.lapidix.dev', title: 'Resume' },
